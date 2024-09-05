667555fc5c6c0be37fcc90efec572a4047df2596
Berl treasure Campomanes <berltreasurecampomanes@Berls-MacBook-Pro.local>
2024-09-05 13:01:45 +0800

    ..

--- app/dswdendorsement/PrintEndorsement.tsx
+++ app/dswdendorsement/PrintEndorsement.tsx
@@ -124,14 +124,10 @@ const PrintEndorsement: React.FC<ChildProps> = ({
               {selectedItem.endorsement_type === 'DSWD' && (
                 <>
                   <div className="uppercase mt-6 font-bold">
-                    RAMIEL A. GUINANDAM
+                    REP. SANCHO FERNANDO "ANDO" F. OAMINAL
                   </div>
-                  <div className="">SWAD Officer</div>
-                  <div className="">
-                    Department of Social Welfare and Development (DSWD)
-                  </div>
-                  <div className="">Ozamiz City Field Office</div>
-                  <div className="mt-6 font-bold">Dear Mr. Guinandam:</div>
+                  <div className="">2nd District, Misamis Occidental</div>
+                  <div className="mt-6 font-bold">Dear Hon. Oaminal:</div>
                 </>
               )}
               {selectedItem.endorsement_type === 'PCSO' && (
