e3683fc06eead614863ccae2882f9f1ad41c7e83
Berl treasure Campomanes <berltreasurecampomanes@Berls-MacBook-Pro.local>
2024-09-20 11:24:58 +0800

    ..

--- app/dswdendorsement/PrintEndorsement.tsx
+++ app/dswdendorsement/PrintEndorsement.tsx
@@ -158,10 +158,11 @@ const PrintEndorsement: React.FC<ChildProps> = ({
                     <span>{gender_address} </span>
                     <span
                       className="font-bold"
-                      style={{
-                        textTransform:
-                          request.length <= 5 ? 'uppercase' : 'lowercase',
-                      }}>
+                      // style={{
+                      //   textTransform:
+                      //     request.length <= 5 ? 'uppercase' : 'lowercase',
+                      // }}
+                    >
                       {request}
                     </span>
                     <span>{patient !== '' && ' of '}</span>
