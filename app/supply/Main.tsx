924d265c507437bfccfe2d7678006b583e933215
Berl treasure Campomanes <berlcamp@gmail.com>
2025-06-26 08:52:45 +0800

    ..

--- app/supply/Main.tsx
+++ app/supply/Main.tsx
@@ -184,7 +184,7 @@ const Page: React.FC = () => {
   const isDataEmpty = !Array.isArray(list) || list.length < 1 || !list
 
   // Check access from permission settings or Super Admins
-  if (!hasAccess('medicine') && !superAdmins.includes(session.user.email))
+  if (!hasAccess('supply') && !superAdmins.includes(session.user.email))
     return <Unauthorized />
 
   return (
